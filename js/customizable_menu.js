590c1829e81c035af7cac6fc7c6cc8917ecbf5ad
gaudy.blanco <gbm2494@hotmail.com>
2017-07-10 16:24:09 -0600

    js fixed

--- js/customizable_menu.js
+++ js/customizable_menu.js
@@ -13,17 +13,3 @@ $body.scrollspy({
     target: '#leftCol',
 	offset: navHeight
 });
-
-/* smooth scrolling sections */
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 50
-        }, 1000);
-        return false;
-      }
-    }
-});
